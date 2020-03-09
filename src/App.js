21876ab9d8907a5df1a160c697e9d641fed53968
Rajdeep-Ghosh-1209 <rajdeepghosh1209@gmail.com>
2020-03-09 18:31:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,150 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { Component } from 'react';
+//import { render } from 'react-dom';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      newItem:"",
+      list:[]
+    }
+  }
+
+  componentDidMount(){
+    this.hydrateStateWithLocalStorage();
+
+    //event listener to save state to localStorage when user leaves/refreshes the page
+
+    window.addEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+
+    //saves if the component has a chance to unmount
+    this.saveStateToLocalStorage();
+  }
+
+  hydrateStateWithLocalStorage(){
+
+    for(let key in this.state){
+
+      if(localStorage.hasOwnProperty(key)){
+        
+        let value =localStorage.getItem(key);
+
+        try
+        {
+            value = JSON.parse(value);
+            this.setState({ [key]: value });
+        } 
+        catch (e) 
+        {
+            this.setState({ [key]: value});   
+        }
+      }
+    }
+  }
+  
+  saveStateToLocalStorage(){
+      for(let key in this.state){
+          localStorage.setItem(key, JSON.stringify(this.state[key]));
+      }
+  }
+
+  updateInput(key, value){
+    //update react state
+    this.setState({
+      [key]: value
+    })
+  }
+
+  addItem(){
+    //create item with unique id
+    const newItem={
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    //copying current list
+    const list = [...this.state.list];
+
+    //add new item to list
+    list.push(newItem);
+
+    //update the set state to go blank after input 
+    this.setState({
+      list,
+      newItem:""
+    });
+
+  }
+
+  deleteItem(id){
+    //copy current list
+    const list = [...this.state.list];
+
+    //filter out the item deleted
+    const updateList = list.filter(item => item.id !== id);
+
+    this.setState({list: updateList});
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <nav>
+        t o D o - L i s t
+        </nav>
+     <div className="container">
+        <div>
+           Add an Item..
+           <br/>
+           <input
+              type="text"
+              className="txt-box"
+              placeholder="Type here.."
+             value= {this.state.newItem}
+              onChange={e => this.updateInput("newItem", e.target.value)}
+            />  
+            <button
+              className="add-btn" 
+              onClick={ () => this.addItem() }
+            >
+             <b>+</b> 
+            </button>
+            <br/>
+            
+              {this.state.list.map(item => {
+                return(
+                   <li key={item.id}>
+                    {item.value}
+                    <button
+                      className="del-btn"
+                      onClick={ () => this.deleteItem(item.id)}
+                    >
+                      <b>X</b>
+                    </button>    
+                  </li>
+                )
+              })}
+            
+
+          </div>
+     </div>
+     </div>
+    );
+  }
+  
 }
 
 export default App;
